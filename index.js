6170489d7fe165a9fa1faa2577f3e707c7b3280d
dipanshupandey <dipanshupandey4488@gmail.com>
2025-08-31 19:12:09 +0530

    created base app

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-const heading=React.createElement("h1",{},"Hello word");
-const root= createRoot(document.getElementById("root"));
-root.render(heading);
