064a6d0f5adaaca73e725bdecf0ace63725d4f4b
dipanshupandey <dipanshupandey4488@gmail.com>
2025-09-12 23:41:38 +0530

    creating our own DB of books

---
+++ src/components/BookReader.js
@@ -0,0 +1,13 @@
+const BookReader = () => {
+  return (
+    <iframe
+      src="/books/Dracula.pdf"
+      width="100%"
+      height="600px"
+      style={{ border: "none" }}
+      title="Dracula"
+    ></iframe>
+  );
+};
+
+export default BookReader;
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,21 +1,11 @@
 import { useState ,useEffect} from "react";
 import Shimmer from "./Shimmer";
+import Books from "./Books";
+
 const Trending=()=>{
     const [trendingBooks,settrendingBooks]=useState([]);
     useEffect(()=>{
-        const fetchTrending=async()=>{
-            try{
-                const res=await fetch("https://openlibrary.org/trending/daily.json");
-                const data=await res.json();
-                console.log(data.works)
-                settrendingBooks(data.works||[]);
-            }
-            catch(error)
-            {
-                console.error(error,"failed fetching books")
-            }
-        }
-        fetchTrending();
+        settrendingBooks(Books);
     },[])
     
     return (
@@ -25,14 +15,14 @@ const Trending=()=>{
             
             {
                 trendingBooks.length>0?trendingBooks.map((book)=>{
-           return  <div className="trending-card" key={book.key}>
+           return  <div className="trending-card" key={book.id}>
            
            <img
-                src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                src={book.cover}
                 alt={book.title}
               />
             <h1 >{book.title?book.title:"title"}</h1>
-            <h2 >{book.author_name?book.author_name:"author"}</h2>
+            <h2 >{book.author?book.author:"author"}</h2>
             
             </div>
                 })
